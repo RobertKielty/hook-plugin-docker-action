dd6403187c7787aab5aaaf8c17e69d9c24694be1
Robert Kielty <rob@ii.coop>
2022-07-24 20:51:21 +0100

    Adds initial files for hook-plugins GH Actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-HMAC_SECRET_FILENAM
-GH_APP_PRIVATE_KEY_PATH
-./bin/hook --github-app-id 221150 --hmac-secret-file hmac --github-app-private-key-path private-key
+#
+HMAC_SECRET_FILENAM="$1"
+GH_APP_PRIVATE_KEY_PATH="$2"
+./bin/hook --github-app-id 221150 --hmac-secret-file ${HMAC_SECRET_FILENAME}--github-app-private-key-path ${GH_APP_PRIVATE_KEY_PATH}
     --config-path config.yaml --plugin-config plugins.yaml --dry-run=false
