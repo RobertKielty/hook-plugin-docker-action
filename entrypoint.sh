34f6f0e033771938d93ca5723afbd17720d19380
Robert Kielty <rob@ii.coop>
2022-08-15 15:09:05 +0100

    Fixes up action input parameters.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,29 @@
 # Input Params : HMAC, PK, HOOK_CONFIG, PLUGIN_CONFIG
 #
 
-#HMAC_SECRET_FILENAME="$1"
-#GH_APP_PRIVATE_KEY_PATH="$2"
-HOOK_CONFIG="$3"
-PLUGIN_CONFIG="$4"
+HMAC="$1"
+GH_APP_ID="$2"
+GH_APP_PK="$3"
+PLUGIN="$4"
 
-echo "$0: Starting hook"
+HOOK_CONFIG="$5"
+PLUGIN_CONFIG="$5"
+
+declare -r PKEY_FILE="./pkey"
+declare -r HMAC_FILE="./hmac"
+
+if [ "${HMAC}" != "" ]; then
+    echo "${HMAC}" > "${HMAC_FILE}"
+fi
+
+if [ "${GH_APP_PK}" != "" ]; then
+    echo "${GH_APP_PK}" > "${PKEY_FILE}"
+fi
+
 #
-# HBD? Would have to consider whther or not to expose this capability
-# to Project Developers.
+# HBD? Would have to consider whether or not to expose this capability to
+# Project Owners. For proof of concept demo purposes lets use ./config.yaml
+# and ./plugins.yaml from this repo.
 #
 if [ "${HOOK_CONFIG}" != "" ]; then
     echo "${HOOK_CONFIG}" > ./config.yaml
@@ -23,15 +37,10 @@ if [ "${PLUGIN_CONFIG}" != "" ]; then
     echo "${PLUGIN_CONFIG}" > ./plugins.yaml
 fi
 
-echo "/ko-app/hook\
-    --plugin ${PLUGIN_CONFIG} \
-    --github-app-id 221150 \
-    --hmac-secret-file ${HMAC_SECRET_FILENAME} \
-    --github-app-private-key-path ${GH_APP_PRIVATE_KEY_PATH} \
-    --config-path config.yaml --plugin-config plugins.yaml --dry-run=false"
-
-/ko-app/hook --plugin-config "${PLUGIN_CONFIG}" \
-    --github-app-id 221150 \
-    --hmac-secret-file "${HMAC_SECRET_FILENAME}" \
-    --github-app-private-key-path "${GH_APP_PRIVATE_KEY_PATH}" \
-    --config-path config.yaml --plugin-config plugins.yaml --dry-run=false
+/ko-app/hook \
+    --hmac-secret-file "${HMAC_FILE}" \
+    --github-app-id "${GH_APP_ID}  \
+    --github-app-private-key-path "${GH_APP_PK}" \
+    --config-path "${HOOK_CONFIG}" \
+    --plugin-config "${PLUGIN_CONFIG}" \
+    --dry-run=false
